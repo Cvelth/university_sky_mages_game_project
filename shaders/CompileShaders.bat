3ff7685ef15f385df75bb58cc78a634658b30675
Vania3690 <Vanya.Krisak@gmail.com>
2017-07-15 17:16:29 -0700

    Shaders project restructurization. .gitignore fix.

--- shaders/CompileShaders.bat
+++
@@ -1,2 +0,0 @@
-C:\Libs\VulkanSDK\1.0.51.0\Bin32\glslangValidator.exe -V CircleShader.vert -o CircleShader.vk.vert.spv
-C:\Libs\VulkanSDK\1.0.51.0\Bin32\glslangValidator.exe -V CoordinateColorShader.frag -o CoordinateColorShader.vk.frag.spv
