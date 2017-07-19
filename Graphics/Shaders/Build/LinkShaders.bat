76cf4f35c5214cc4b643387bb730c070dda9b019
Cvelth <Cvelth.mail@gmail.com>
2017-07-18 21:41:59 -0700

    Shaders Link System is now suitable for use in x64 build.

--- Graphics/Shaders/Build/LinkShaders.bat
+++ Graphics/Shaders/Build/LinkShaders.bat
@@ -1,14 +1,24 @@
 mkdir ..\..\Release\shaders\ 1>NUL 2>NUL
 mkdir ..\..\Debug\shaders\ 1>NUL 2>NUL
+mkdir ..\..\x64\Release\shaders\ 1>NUL 2>NUL
+mkdir ..\..\x64\Debug\shaders\ 1>NUL 2>NUL
 
 copy Output\Circle.vk.vert.spv ..\..\Release\shaders\Circle.vk.vert.spv 1>NUL
 copy Output\Circle.vk.vert.spv ..\..\Debug\shaders\Circle.vk.vert.spv 1>NUL
+copy Output\Circle.vk.vert.spv ..\..\x64\Release\shaders\Circle.vk.vert.spv 1>NUL
+copy Output\Circle.vk.vert.spv ..\..\x64\Debug\shaders\Circle.vk.vert.spv 1>NUL
 
 copy Output\Triangle.vk.vert.spv ..\..\Release\shaders\Triangle.vk.vert.spv 1>NUL
 copy Output\Triangle.vk.vert.spv ..\..\Debug\shaders\Triangle.vk.vert.spv 1>NUL
+copy Output\Triangle.vk.vert.spv ..\..\x64\Release\shaders\Triangle.vk.vert.spv 1>NUL
+copy Output\Triangle.vk.vert.spv ..\..\x64\Debug\shaders\Triangle.vk.vert.spv 1>NUL
 
 copy Output\CoordinateColor.vk.frag.spv ..\..\Release\shaders\CoordinateColor.vk.frag.spv 1>NUL
 copy Output\CoordinateColor.vk.frag.spv ..\..\Debug\shaders\CoordinateColor.vk.frag.spv 1>NUL
+copy Output\CoordinateColor.vk.frag.spv ..\..\x64\Release\shaders\CoordinateColor.vk.frag.spv 1>NUL
+copy Output\CoordinateColor.vk.frag.spv ..\..\x64\Debug\shaders\CoordinateColor.vk.frag.spv 1>NUL
 
 copy Output\Triangle.vk.frag.spv ..\..\Release\shaders\Triangle.vk.frag.spv 1>NUL
 copy Output\Triangle.vk.frag.spv ..\..\Debug\shaders\Triangle.vk.frag.spv 1>NUL
+copy Output\Triangle.vk.frag.spv ..\..\x64\Release\shaders\Triangle.vk.frag.spv 1>NUL
+copy Output\Triangle.vk.frag.spv ..\..\x64\Debug\shaders\Triangle.vk.frag.spv 1>NUL
