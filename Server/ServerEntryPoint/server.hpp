cab10427f8dff77388709c44e8d4464dc3ba3920
Cvelth <Cvelth.mail@gmail.com>
2018-07-31 18:44:17 +0300

    server_main and client_main were moved to cpp files.

--- Server/ServerEntryPoint/server.hpp
+++ Server/ServerEntryPoint/server.hpp
@@ -1,7 +1,2 @@
 #pragma once
-#include "../../Engines/ObjectStorage/Objects.hpp"
-#include "../../Engines/ObjectStorage/Settings.hpp"
-int server_main(int argc, char **argv) {
-	auto objects = initialize_object_storage(ProgramMode::Server);
-	return 0;
-}
+int server_main(int argc, char **argv);
