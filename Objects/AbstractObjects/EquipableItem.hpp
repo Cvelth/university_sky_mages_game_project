b785c0e5a13c6d0e8f1240727b27163585b057a5
Cvelth <Cvelth.mail@gmail.com>
2018-08-03 13:41:33 +0300

    EquipableItem's now store its name and descripton.

--- Objects/AbstractObjects/EquipableItem.hpp
+++ Objects/AbstractObjects/EquipableItem.hpp
@@ -1,8 +1,13 @@
 #pragma once
+#include <string>
 #include "Objects/ObjectState/DependentObject.hpp"
 class EquipableItem : public DependentObjectState {
+	std::string m_name;
+	std::string m_description;
 public:
-	EquipableItem(float mass)
-		: DependentObjectState(mass) {}
+	EquipableItem(float mass, std::string &name, std::string &description)
+		: DependentObjectState(mass), m_name(name), m_description(description) {}
 	~EquipableItem() {}
+	inline std::string const& name() const { return m_name; }
+	inline std::string const& description() const { return m_description; }
 };
