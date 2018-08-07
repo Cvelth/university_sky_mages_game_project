b30450250204297c84711a593f2fab5002712bef
Cvelth <Cvelth.mail@gmail.com>
2018-08-08 02:35:13 +0300

    Minor fixes were applied to MapStorage interface.

--- Engines/ObjectStorage/MapStorage.hpp
+++ Engines/ObjectStorage/MapStorage.hpp
@@ -4,10 +4,10 @@
 #include "../../Objects/Map/Map.hpp"
 class MapStorage : protected FileLoader {
 protected:
-	virtual void parse_line(std::string const& line) override { static_assert("Hasn't been implemented yet."); }
+	[[deprecated]] virtual void parse_line(std::string const& line) override {}
 	virtual void parse_file_type_info(std::string const& line) override;
 	virtual std::string generate_file_type_info() override { return "with a Map"; }
 public:
 	virtual void save(Map *map, std::string const& filename, std::string const& path);
-	virtual Map* load_map(std::string const& filename, std::string const& path) { static_assert("Hasn't been implemented yet."); return nullptr; };
+	virtual Map* load(std::string const& filename, std::string const& path, bool use = true);
 };
