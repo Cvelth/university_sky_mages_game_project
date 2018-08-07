ba39f84202cb12964cf7483755d8320e10966f3a
Cvelth <Cvelth.mail@gmail.com>
2018-08-08 01:13:03 +0300

    MapStorage interface was mapped.

--- Engines/ObjectStorage/MapStorage.cpp
+++ Engines/ObjectStorage/MapStorage.cpp
@@ -1,1 +1,21 @@
 #include "MapStorage.hpp"
+void MapStorage::parse_file_type_info(std::string const& line) {
+	if (line != "with a Map")
+		throw Exceptions::FileParsingException("File seems to be corrupted");
+}
+#include <fstream>
+void MapStorage::save(Map *map, std::string const& filename, std::string const& path) {
+	std::ofstream f;
+	f.open(path + filename + MapFileExtention);
+	if (!f)
+		throw Exceptions::FileCannotBeOpennedException("It's impossible to write in file. Some kind of system error");
+
+	f << generate_first_line(filename, MapFileExtention) << std::endl;
+
+	f << "BlockData\n";
+	for (auto &it : map->get_blocks_data())
+		f << "Block here\n";
+
+	f << "CellData\n";
+
+}
--- Engines/ObjectStorage/MapStorage.hpp
+++ Engines/ObjectStorage/MapStorage.hpp
@@ -1,8 +1,13 @@
 #pragma once
 #include "FileLoader.hpp"
+#include "DefaultObjectStorageData.hpp"
+#include "../../Objects/Map/Map.hpp"
 class MapStorage : protected FileLoader {
-private:
-
+protected:
+	virtual void parse_line(std::string const& line) override { static_assert("Hasn't been implemented yet."); }
+	virtual void parse_file_type_info(std::string const& line) override;
+	virtual std::string generate_file_type_info() override { return "with a Map"; }
 public:
-
+	virtual void save(Map *map, std::string const& filename, std::string const& path);
+	virtual Map* load_map(std::string const& filename, std::string const& path) { static_assert("Hasn't been implemented yet."); return nullptr; };
 };
