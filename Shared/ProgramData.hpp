334073510c6a8e35202feba60c7349a59e01267d
Cvelth <Cvelth.mail@gmail.com>
2018-08-08 12:11:16 +0300

    Program version is now v0.2.3.249_dev.

--- Shared/ProgramData.hpp
+++ Shared/ProgramData.hpp
@@ -3,6 +3,6 @@
 const std::string Program_Name = "SkyMages";
 const unsigned int Program_Major_Version = 0u;
 const unsigned int Program_Minor_Version = 2u;
-const unsigned int Program_Patch_Version = 2u;
-const unsigned int Program_Build_Version = 222u;
+const unsigned int Program_Patch_Version = 3u;
+const unsigned int Program_Build_Version = 249u;
 const std::string Program_Version_Suffix = "dev";
