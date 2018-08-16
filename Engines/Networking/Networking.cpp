e80073d0184fc5f9a98318fc2db0e434d88741e6
Cvelth <Cvelth.mail@gmail.com>
2018-08-16 16:46:04 +0300

    Multiple host issue was fixed.

--- Engines/Networking/Networking.cpp
+++ Engines/Networking/Networking.cpp
@@ -6,16 +6,11 @@
 size_t const client_number = 9;
 size_t const channel_number = 3;
 
-bool is_server = false;
-bool is_client = false;
-
-ENetHost *host;
+ENetHost *client_host;
+ENetHost *server_host;
 ENetPeer *server_peer = nullptr;
 
 void initialize_server(bool const& should_close, std::function<void()> peer_connected, std::function<void()> peer_disconnected, std::function<void(std::string)> packet_received, size_t port) {
-	if (is_server || is_client) throw Exceptions::NetworkingException("Cannot initialize server. NetworkingEngine was already initialized.");
-	is_server = true;
-
 	if (port > std::numeric_limits<uint16_t>::max())
 		throw Exceptions::NetworkingException("Unsupported 'port' value was passed.");
 
@@ -27,13 +22,13 @@ void initialize_server(bool const& should_close, std::function<void()> peer_conn
 		address.host = ENET_HOST_ANY;
 		address.port = static_cast<uint16_t>(port);
 
-		host = enet_host_create(&address, client_number, channel_number, 0, 0);
-		if (host == NULL)
+		server_host = enet_host_create(&address, client_number, channel_number, 0, 0);
+		if (server_host == NULL)
 			throw Exceptions::NetworkingException("Server host cannot be initialized.");
 
 		ENetEvent event;
 		while (!should_close) {
-			enet_host_service(host, &event, 1000);
+			enet_host_service(server_host, &event, 1000);
 			switch (event.type) {
 				case ENET_EVENT_TYPE_CONNECT:
 					peer_connected();
@@ -52,17 +47,13 @@ void initialize_server(bool const& should_close, std::function<void()> peer_conn
 			}
 		}
 
-		enet_host_destroy(host);
+		enet_host_destroy(server_host);
 		enet_deinitialize();
 	});
 	thread.detach();
-	is_server = false;
 }
 
 void initialize_client(bool const& should_close, std::function<void(std::string)> packet_received, std::string ip, size_t port) {
-	if (is_server || is_client) throw Exceptions::NetworkingException("Cannot initialize client. NetworkingEngine was already initialized.");
-	is_client = true;
-
 	if (port > std::numeric_limits<uint16_t>::max())
 		throw Exceptions::NetworkingException("Unsupported 'port' value was passed.");
 
@@ -70,20 +61,20 @@ void initialize_client(bool const& should_close, std::function<void(std::string)
 		if (enet_initialize() != 0)
 			throw Exceptions::NetworkingException("Enet library cannot be initialized.");
 
-		host = enet_host_create(NULL, 1, channel_number, 0, 0);
-		if (host == NULL)
+		client_host = enet_host_create(NULL, 1, channel_number, 0, 0);
+		if (client_host == NULL)
 			throw Exceptions::NetworkingException("Client host cannot be initialized.");
 
 		ENetAddress address;
 		enet_address_set_host(&address, ip.c_str());
 		address.port = static_cast<uint16_t>(port);
 
-		server_peer = enet_host_connect(host, &address, channel_number, 0);
+		server_peer = enet_host_connect(client_host, &address, channel_number, 0);
 		if (server_peer == NULL)
 			throw Exceptions::NetworkingException("Connection to cannot be established.");
 
 		ENetEvent event;
-		if (enet_host_service(host, &event, 5000) > 0 && event.type == ENET_EVENT_TYPE_CONNECT) {
+		if (enet_host_service(client_host, &event, 5000) > 0 && event.type == ENET_EVENT_TYPE_CONNECT) {
 			//connection was successful.
 		} else {
 			enet_peer_reset(server_peer);
@@ -91,7 +82,7 @@ void initialize_client(bool const& should_close, std::function<void(std::string)
 		}
 		
 		while (!should_close) {
-			enet_host_service(host, &event, 1000);
+			enet_host_service(client_host, &event, 1000);
 			switch (event.type) {
 				case ENET_EVENT_TYPE_DISCONNECT:
 					throw Exceptions::ConnectionException(("Disconnected from " + ip).c_str());
@@ -106,7 +97,7 @@ void initialize_client(bool const& should_close, std::function<void(std::string)
 		}
 		bool was_successful = false;
 		enet_peer_disconnect(server_peer, 0);
-		while (enet_host_service(host, &event, 3000) > 0) {
+		while (enet_host_service(client_host, &event, 3000) > 0) {
 			switch (event.type) {
 				case ENET_EVENT_TYPE_RECEIVE:
 					packet_received(reinterpret_cast<char*>(event.packet->data));
@@ -119,22 +110,17 @@ void initialize_client(bool const& should_close, std::function<void(std::string)
 		}
 		if (!was_successful) enet_peer_reset(server_peer);
 		server_peer = nullptr;
-		enet_host_destroy(host);
+		enet_host_destroy(client_host);
 		enet_deinitialize();
 	});
 	thread.detach();
-	is_client = false;
 }
 
 void send_to_server(std::string const& data, size_t channel_id, bool important) {
-	if (!is_client) throw Exceptions::NetworkingException("Client wasn't initialized. Can't send data to server.");
-
 	enet_peer_send(server_peer, static_cast<uint8_t>(channel_id), enet_packet_create(data.data(), data.size() + 1, important ? ENET_PACKET_FLAG_RELIABLE : 0));
-	enet_host_flush(host);
+	enet_host_flush(client_host);
 }
 void bcast_from_server(std::string const& data, size_t channel_id, bool important) {
-	if (!is_server) throw Exceptions::NetworkingException("Server wasn't initialized. Can't brodcast data.");
-
-	enet_host_broadcast(host, static_cast<uint8_t>(channel_id), enet_packet_create(data.data(), data.size() + 1, important ? ENET_PACKET_FLAG_RELIABLE : 0));
-	enet_host_flush(host);
+	enet_host_broadcast(server_host, static_cast<uint8_t>(channel_id), enet_packet_create(data.data(), data.size() + 1, important ? ENET_PACKET_FLAG_RELIABLE : 0));
+	enet_host_flush(server_host);
 }
