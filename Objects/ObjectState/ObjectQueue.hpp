ffd82e6197b26f3a502984ed188f79540c21a683
Cvelth <Cvelth.mail@gmail.com>
2018-08-17 18:24:36 +0300

    ObjectQueue was refactored to allow different inner container types. set (legacy), list and vector are supported as of now.

--- Objects/ObjectState/ObjectQueue.hpp
+++ Objects/ObjectState/ObjectQueue.hpp
@@ -1,23 +1,17 @@
 #pragma once
-#include <set>
 #include <functional>
 #include <string>
 #include <sstream>
-#include <memory>
-
 template <typename Type>
-class AbstractQueue {
-private:
-	std::set<std::shared_ptr<Type>> m_queue;
+class AbstractQueueInterface {
 public:
-	AbstractQueue() {}
-	~AbstractQueue() {}
+	virtual void add(Type* object) abstract;
+	virtual void remove(Type* object) abstract;
+	virtual void for_each(const std::function<void(Type*)> &lambda) abstract;
+	virtual void for_each(const std::function<void(Type*)> &lambda) const abstract;
+	virtual size_t size() const abstract;
 
-	void add(Type* object) { m_queue.insert(std::shared_ptr<Type>(object)); }
-	void remove(Type* object) { m_queue.erase(std::shared_ptr<Type>(object)); }
-	void for_each(const std::function<void(Type*)> &lambda) { for (auto &it : m_queue) lambda(&*it); }
-	void for_each(const std::function<void(Type*)> &lambda) const { for (auto &it : m_queue) lambda(&*it); }
-	std::string to_string() const {
+	virtual std::string to_string() const {
 		std::ostringstream s;
 		s << "Queue ";
 		for_each([&s](Type *it) {
@@ -25,7 +19,43 @@ public:
 		});
 		return s.str();
 	}
-	size_t size() const { return m_queue.size(); }
+};
+#include <memory>
+#include <set>
+template <typename Type>
+class AbstractSet : public AbstractQueueInterface<Type> {
+private:
+	std::set<std::shared_ptr<Type>> m_queue;
+public:
+	virtual void add(Type* object) override { m_queue.insert(std::shared_ptr<Type>(object)); }
+	virtual void remove(Type* object) override { m_queue.erase(std::shared_ptr<Type>(object)); }
+	virtual void for_each(const std::function<void(Type*)> &lambda) override { for (auto &it : m_queue) lambda(&*it); }
+	virtual void for_each(const std::function<void(Type*)> &lambda) const override { for (auto &it : m_queue) lambda(&*it); }
+	virtual size_t size() const override { return m_queue.size(); }
+};
+#include <list>
+template <typename Type>
+class AbstractList : public AbstractQueueInterface<Type> {
+private:
+	std::list<std::shared_ptr<Type>> m_queue;
+public:
+	virtual void add(Type* object) override { m_queue.push_back(std::shared_ptr<Type>(object)); }
+	virtual void remove(Type* object) override { m_queue.erase(std::find(m_queue.begin(), m_queue.end(), std::shared_ptr<Type>(object))); }
+	virtual void for_each(const std::function<void(Type*)> &lambda) override { for (auto &it : m_queue) lambda(&*it); }
+	virtual void for_each(const std::function<void(Type*)> &lambda) const override { for (auto &it : m_queue) lambda(&*it); }
+	virtual size_t size() const override { return m_queue.size(); }
+};
+#include <vector>
+template <typename Type>
+class AbstractVector : public AbstractQueueInterface<Type> {
+private:
+	std::vector<std::shared_ptr<Type>> m_queue;
+public:
+	virtual void add(Type* object) override { m_queue.push_back(std::shared_ptr<Type>(object)); }
+	virtual void remove(Type* object) override { m_queue.erase(std::find(m_queue.begin(), m_queue.end(), std::shared_ptr<Type>(object))); }
+	virtual void for_each(const std::function<void(Type*)> &lambda) override { for (auto &it : m_queue) lambda(&*it); }
+	virtual void for_each(const std::function<void(Type*)> &lambda) const override { for (auto &it : m_queue) lambda(&*it); }
+	virtual size_t size() const override { return m_queue.size(); }
 };
 
 #define DefineNewQueue(QueueType, Type) \
@@ -34,10 +64,11 @@ public:
 	};
 
 class IndependentObject;
-DefineNewQueue(ObjectQueue, IndependentObject);
 class ShootableObject;
-DefineNewQueue(ProjectileQueue, ShootableObject);
-class Actor;
-DefineNewQueue(ActorQueue, Actor);
 class MainActor;
-DefineNewQueue(MainActorQueue, MainActor);
+class ObjectQueue : public AbstractSet<IndependentObject> { public: using AbstractSet::AbstractSet; };
+class ProjectileQueue : public AbstractSet<ShootableObject> { public: using AbstractSet::AbstractSet; };
+class MainActorQueue : public AbstractVector<MainActor> { public: using AbstractVector::AbstractVector; };
+
+#include "Objects/Actors/MainActor.hpp"
+#include "Objects/AbstractObjects/ShootableObject.hpp"
