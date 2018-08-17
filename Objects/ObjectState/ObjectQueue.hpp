a344f5fb8e9c09a6764651e71bbc1acba95f5b9f
Cvelth <Cvelth.mail@gmail.com>
2018-08-17 17:06:30 +0300

    AbstractQueue was refactored. Few methods were added:  - constant for_each();  - to_string();  - size();

--- Objects/ObjectState/ObjectQueue.hpp
+++ Objects/ObjectState/ObjectQueue.hpp
@@ -1,18 +1,31 @@
 #pragma once
 #include <set>
 #include <functional>
+#include <string>
+#include <sstream>
+#include <memory>
 
 template <typename Type>
 class AbstractQueue {
 private:
-	std::set<Type*> m_queue;
+	std::set<std::shared_ptr<Type>> m_queue;
 public:
 	AbstractQueue() {}
 	~AbstractQueue() {}
 
-	void add(Type* object) { m_queue.insert(object); }
-	void remove(Type* object) { m_queue.erase(object); }
-	void for_each(const std::function<void(Type*)>& lambda) { for (auto it : m_queue) lambda(it); }
+	void add(Type* object) { m_queue.insert(std::shared_ptr<Type>(object)); }
+	void remove(Type* object) { m_queue.erase(std::shared_ptr<Type>(object)); }
+	void for_each(const std::function<void(Type*)> &lambda) { for (auto &it : m_queue) lambda(&*it); }
+	void for_each(const std::function<void(Type*)> &lambda) const { for (auto &it : m_queue) lambda(&*it); }
+	std::string to_string() const {
+		std::ostringstream s;
+		s << "Queue ";
+		for_each([&s](Type *it) {
+			s << std::string(*it);
+		});
+		return s.str();
+	}
+	size_t size() const { return m_queue.size(); }
 };
 
 #define DefineNewQueue(QueueType, Type) \
