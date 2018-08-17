da113ec087f6c512a720dbaad33de51cc78cd077
Cvelth <Cvelth.mail@gmail.com>
2018-08-17 19:27:26 +0300

    Client.hpp was refactored.

--- Client/ClientEntryPoint/client.hpp
+++ Client/ClientEntryPoint/client.hpp
@@ -1,15 +1,16 @@
 #pragma once
 #include "Shared/AbstractException.hpp"
 #include "Engines/ObjectStorage/Objects.hpp"
-void game_process(Objects *s);
+void game_process(Objects *s, size_t &client_index);
 int client_main() {
+	size_t client_index = -1;
 	Objects *o = initialize_object_storage(ProgramMode::Client);
 	initialize_render_info();
 	try {
-		game_process(o);
+		game_process(o, client_index);
 	}
-	catch (Exceptions::AbstractException& e) {
-		e.print();
+	catch (std::exception& e) {
+		e.what();
 		getchar(); // Prevents Program from closing.
 	}
 	delete o;
@@ -17,25 +18,17 @@ int client_main() {
 }
 
 #include "Shared/GameMode.hpp"
-#include "Client/Window/Window.hpp"
 #include "Client/Controller/ControllerInterface.hpp"
-#include "Engines/Physics/PhysicsEngine.hpp"
-#include "Engines/Camera/Camera.hpp"
-#include "Objects/Map/Map.hpp"
-#include "Engines/ObjectStorage/MapStorage.hpp"
-#include "Objects/Actors/MainActor.hpp"
-#include "Objects/EquipableItems/EnergyStorage.hpp"
-#include "Objects/EquipableItems/FlyEngine.hpp"
-#include "Objects/EquipableItems/Weapon.hpp"
-#include "Engines/Graphics/HUD_RenderInfo.hpp"
-#include "Objects/ObjectState/ObjectQueue.hpp"
 #include "Engines/ObjectStorage/Settings.hpp"
+#include "Objects/ObjectState/ObjectQueue.hpp"
+#include "Client/Window/Window.hpp"
+#include "Engines/Physics/PhysicsEngine.hpp"
 #include "Engines/Networking/Networking.hpp"
-
-#include <thread>
-#include <sstream>
-
-void game_process(Objects *o) {
+#include "Engines/ObjectStorage/MapStorage.hpp"
+#include "Engines/Camera/Camera.hpp"
+#include "Objects/Actors/MainActor.hpp"
+#include "Engines/Graphics/HUD_RenderInfo.hpp"
+void game_process(Objects *o, size_t &client_index) {
 	GameModeController::startInitialization();
 	ControllerInterface* controller = new ControllerInterface();
 	auto keys = o->settings()->getKeysValue("Keys_Layout");
@@ -46,10 +39,10 @@ void game_process(Objects *o) {
 	auto miscellaneous_object_queue = new ObjectQueue();
 
 	Window* window = new Window(o->get_program_version().c_str(),
-		o->settings()->getUintValue("Screen_Width"),
-		o->settings()->getUintValue("Screen_Height"),
-		o->settings()->getBoolValue("Fullscreen_Window"),
-		main_object_queue, projectile_queue, miscellaneous_object_queue);
+								o->settings()->getUintValue("Screen_Width"),
+								o->settings()->getUintValue("Screen_Height"),
+								o->settings()->getBoolValue("Fullscreen_Window"),
+								main_object_queue, projectile_queue, miscellaneous_object_queue);
 	window->insertController(controller);
 	window->changeUpdateInterval(1'000'000 / o->settings()->getUintValue("Graphical_Updates_Per_Second"));
 
@@ -58,36 +51,43 @@ void game_process(Objects *o) {
 	}, main_object_queue, projectile_queue, miscellaneous_object_queue);
 	physics_engine->changeUpdateInterval(1'000'000 / o->settings()->getUintValue("Physical_Updates_Per_Second"));
 
-	MainActor *main_actor = new MainActor(60.f, 2.f, 4.f, 40.f, 30.f);
-	auto energy_storage = o->get_energy_storage();
-	main_actor->giveEnergyStorage(energy_storage);
-	main_actor->giveFlyEngine(o->get_fly_engine());
-	main_actor->giveRightWeapon(o->get_weapon());
-
-	controller->setMainActor(main_actor);
-	main_object_queue->add(main_actor);
-
-	Camera *camera = nullptr;
+	Map *map;
 	auto networking_thread = initialize_client([&window](void) {
 		return window->isWindowClosed();
-	}, [&window, &physics_engine, &camera, &main_actor](std::string const& data) /*packed received*/ {
+	}, [&window, &physics_engine, &map, &client_index, &main_object_queue, &o](std::string const& data) /*packed received*/ {
 		std::istringstream s(data);
 		std::string string;
 		std::getline(s, string);
-		if (string == "Map") {
-			Map *map = MapStorage::string_to_map(data.substr(string.size() + 1));
+		if (string == "Index") {
+			s >> client_index;
+		} else if (string == "Map") {
+			map = MapStorage::string_to_map(data.substr(string.size() + 1));
 			if (!map)
 				throw Exceptions::NetworkingException(("Received map is corrupted: " + data).c_str());
-			physics_engine->initializeCollisionSystem(map);
-
-			if (camera) delete camera;
-			Camera *camera = new Camera(map, main_actor, window->currentAspectRatio(), 100.f);
-			window->insertCamera(camera);
+		} else if (string == "MainActorQueue") {
+			std::getline(s, string);
+			if (string != "Queue")
+				throw Exceptions::NetworkingException(("Received data is corrupted: " + data).c_str());
+			while (std::getline(s, string)) {
+				main_object_queue->add(string_to_main_actor(string, o));
+			}
 		} else
 			throw Exceptions::NetworkingException(("Unsupported data was received from server: " + data).c_str());
 	}, o->settings()->getStringValue("Server_IP"));
-	
-	auto hud = new HUD_RenderInfo(energy_storage);
+
+	//Waiting for initial server answer.
+	while (client_index == -1 || !map || main_object_queue->size() == 0)
+		std::this_thread::sleep_for(std::chrono::microseconds(window->getUpdateInterval()));
+
+	MainActor *main_actor = &*main_object_queue->at(client_index);
+
+	physics_engine->initializeCollisionSystem(map);
+	Camera *camera = new Camera(map, main_actor, window->currentAspectRatio(), 100.f);
+	window->insertCamera(camera);
+
+	controller->setMainActor(main_actor);
+
+	auto hud = new HUD_RenderInfo(main_actor->energy_storage());
 	window->insertHUDRenderInfo(hud);
 
 	std::thread physics_thread(&PhysicsEngine::loop, physics_engine, false);
@@ -97,7 +97,6 @@ void game_process(Objects *o) {
 
 	delete hud;
 	delete camera;
-	delete main_actor;
 	delete physics_engine;
 	delete window;
 	delete main_object_queue;
