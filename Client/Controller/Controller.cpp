94061f5811130f839c63670cb6418e9bbde65d92
Cvelth <Cvelth.mail@gmail.com>
2018-08-18 14:30:40 +0300

    Controller now sends events on every engine-dependent action using NetworkController.

--- Client/Controller/Controller.cpp
+++ Client/Controller/Controller.cpp
@@ -6,15 +6,12 @@
 #include "Shared/GameMode.hpp"
 
 Controller::Controller() : m_controlledCamera(nullptr) {}
-
 Controller::~Controller() {}
-
 
 void Controller::mouseScrollEvent(GLFWwindow* w, double x, double y) {
 	if (m_controlledCamera)
 		m_controlledCamera->changeZoom(1.f - float(y) / 10.f);
 }
-
 void Controller::resizeEvent(GLFWwindow *w, int x, int y) {
 	current_screen_width = x;
 	current_screen_height = y;
@@ -26,70 +23,81 @@ void Controller::resizeEvent(GLFWwindow *w, int x, int y) {
 	}
 }
 
+void Controller::startCameraControl(Camera* camera) {
+	m_controlledCamera = camera;
+}
+void Controller::stopCameraControl() {
+	m_controlledCamera = nullptr;
+}
+void Controller::setMainActor(MainActor* actor) {
+	m_actor = actor;
+}void Controller::removeMainActor() {
+	m_actor = nullptr;
+}
+void Controller::startKeyControl(KeyLayout const *keys) {
+	m_keys = keys;
+}
+void Controller::stopKeyControl() {
+	m_keys = nullptr;
+}
+
+#include "Engines/Networking/NetworkController.hpp"
 void Controller::keyEvent(GLFWwindow *w, mgl::Key key, int scancode, mgl::KeyAction action, mgl::Mods mods) {
 	if (m_keys) {
 		if (action == mgl::KeyAction::press) {
-			if (key == *m_keys->move_up)
+			if (key == *m_keys->move_up) {
+				NetworkController::send_control_event(NetworkController::ControlEvent::move_up, true);
 				m_actor->m_engine->accelerate_up(false);
-			else if (key == *m_keys->move_down)
+			} else if (key == *m_keys->move_down) {
+				NetworkController::send_control_event(NetworkController::ControlEvent::move_down, true);
 				m_actor->m_engine->accelerate_down(false);
-			else if (key == *m_keys->move_left)
+			} else if (key == *m_keys->move_left) {
+				NetworkController::send_control_event(NetworkController::ControlEvent::move_left, true);
 				m_actor->m_engine->accelerate_left(false);
-			else if (key == *m_keys->move_right)
+			} else if (key == *m_keys->move_right) {
+				NetworkController::send_control_event(NetworkController::ControlEvent::move_right, true);
 				m_actor->m_engine->accelerate_right(false);
-			else if (key == *m_keys->anti_gravity)
-				if (m_actor->m_engine->is_auti_gravity_turned_on())
-					m_actor->m_engine->turn_off_anti_gravity();
-				else
-					m_actor->m_engine->turn_on_anti_gravity(m_actor->mass());
-			else if (key == *m_keys->pause)
+			} else if (key == *m_keys->anti_gravity) {
+				NetworkController::send_control_event(NetworkController::ControlEvent::anti_gravity);
+				m_actor->m_engine->activate_anti_gravity(m_actor->mass());
+			} else if (key == *m_keys->pause) {
 				GameModeController::pauseGame();
+			}
 		} else if (action == mgl::KeyAction::release) {
-			if (key == *m_keys->move_up)
+			if (key == *m_keys->move_up) {
+				NetworkController::send_control_event(NetworkController::ControlEvent::move_up, false);
 				m_actor->m_engine->accelerate_up(true);
-			else if (key == *m_keys->move_down)
+			} else if (key == *m_keys->move_down) {
+				NetworkController::send_control_event(NetworkController::ControlEvent::move_down, false);
 				m_actor->m_engine->accelerate_down(true);
-			else if (key == *m_keys->move_left)
+			} else if (key == *m_keys->move_left) {
+				NetworkController::send_control_event(NetworkController::ControlEvent::move_left, false);
 				m_actor->m_engine->accelerate_left(true);
-			else if (key == *m_keys->move_right)
+			} else if (key == *m_keys->move_right) {
+				NetworkController::send_control_event(NetworkController::ControlEvent::move_right, false);
 				m_actor->m_engine->accelerate_right(true);
+			}
 		}
 	}
 }
 void Controller::mouseButtonEvent(GLFWwindow *w, mgl::MouseButton button, mgl::MouseAction action, mgl::Mods mods) {
 	if (action == mgl::MouseAction::press) {
-		if (button == mgl::MouseButton::left)
+		if (button == mgl::MouseButton::left) {
+			NetworkController::send_control_event(NetworkController::ControlEvent::left_weapon, true);
 			m_actor->activateRightWeapon();
-		else if (button == mgl::MouseButton::right)
+		} else if (button == mgl::MouseButton::right) {
+			NetworkController::send_control_event(NetworkController::ControlEvent::right_weapon, true);
 			m_actor->activateLeftWeapon();
-	} else if (action == mgl::MouseAction::release)
-		if (button == mgl::MouseButton::left)
+		}
+	} else if (action == mgl::MouseAction::release) {
+		if (button == mgl::MouseButton::left) {
+			NetworkController::send_control_event(NetworkController::ControlEvent::left_weapon, false);
 			m_actor->deactivateRightWeapon();
-		else if (button == mgl::MouseButton::right)
+		} else if (button == mgl::MouseButton::right) {
+			NetworkController::send_control_event(NetworkController::ControlEvent::right_weapon, false);
 			m_actor->deactivateLeftWeapon();
-}
-
-void Controller::startCameraControl(Camera* camera) {
-	m_controlledCamera = camera;
-}
-
-void Controller::stopCameraControl() {
-	m_controlledCamera = nullptr;
-}
-
-void Controller::setMainActor(MainActor* actor) {
-	m_actor = actor;
-}
-void Controller::removeMainActor() {
-	m_actor = nullptr;
-}
-
-void Controller::startKeyControl(KeyLayout const *keys) {
-	m_keys = keys;
-}
-
-void Controller::stopKeyControl() {
-	m_keys = nullptr;
+		}
+	}
 }
 
 #include "../MyGraphicsLibrary/MGL/OpenGL/FunctionsMirror/ExperimentalFunctionsMirror.hpp"
