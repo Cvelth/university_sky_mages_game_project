6a9b79cd77f35636e79474342a8b328c24a3f4f0
Cvelth <Cvelth.mail@gmail.com>
2018-08-18 21:43:07 +0300

    Camera now uses std::shared_ptr to store map and center_figure.

--- Client/Controller/Camera.hpp
+++ Client/Controller/Camera.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <memory>
 namespace mgl {
 	namespace math {
 		class vector;
@@ -8,21 +9,22 @@ class Map;
 class Actor;
 class Camera {
 private:
-	Map *m_map;
+	std::shared_ptr<Map> m_map;
+	std::shared_ptr<Actor> m_center_figure;
 
 	mgl::math::vector *m_center;
-	Actor *m_center_figure;
 
 	float m_horizontal_blocks_number;
 	float m_aspect_ratio;
 
 	bool m_camera_was_changed;
+	bool m_map_was_changed;
 protected:
 	void correct_height();
 	void correct_width();
 	void correct();
 public:
-	Camera(Map *map, Actor *center_figure, float aspectRatio, float blocks = 100);
+	Camera(std::shared_ptr<Map> map, std::shared_ptr<Actor> center_figure, float aspectRatio, float blocks = 100);
 
 	void changeAspectRatio(float aspectRatio);
 	void changeZoom(float magnifier);
@@ -31,7 +33,7 @@ public:
 	void move(float x, float y);
 	void move(mgl::math::vector const& point);
 	void move();
-	void changeCenterFigure(Actor *center_figure);
+	void changeCenterFigure(std::shared_ptr<Actor> center_figure);
 
 	inline float aspectRatio() const {
 		return m_aspect_ratio;
@@ -46,13 +48,19 @@ public:
 	long long minY_i() const;
 	long long maxX_i() const;
 	long long maxY_i() const;
-	Map* map();
+	std::shared_ptr<Map> map();
 
 	inline bool wasCameraChanged() const {
 		return m_camera_was_changed;
 	}
 	inline void cameraChangeWasHandled() {
 		m_camera_was_changed = false;
+	}
+	inline bool wasMapChanged() const {
+		return m_map_was_changed;
+	}
+	inline void mapChangeWasHandled() {
+		m_map_was_changed = false;
 	}
 
 	~Camera();
