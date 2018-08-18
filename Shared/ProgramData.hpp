cebf5aa0e73fe2614f7c5674ec67869b21768db8
Cvelth <Cvelth.mail@gmail.com>
2018-08-19 00:06:12 +0300

    Game version is now v0.3.1.348_dev.

--- Shared/ProgramData.hpp
+++ Shared/ProgramData.hpp
@@ -3,6 +3,6 @@
 const std::string Program_Name = "SkyMages";
 const unsigned int Program_Major_Version = 0u;
 const unsigned int Program_Minor_Version = 3u;
-const unsigned int Program_Patch_Version = 0u;
-const unsigned int Program_Build_Version = 300u;
+const unsigned int Program_Patch_Version = 1u;
+const unsigned int Program_Build_Version = 348u;
 const std::string Program_Version_Suffix = "dev";
