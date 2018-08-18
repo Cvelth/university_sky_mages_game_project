8b6c61f010172fb73e041021153d3fc82513b7b4
Cvelth <Cvelth.mail@gmail.com>
2018-08-18 15:57:00 +0300

    Controller now depends on GameState. For example, it's now able to react differently on the same events during normal and pause states.

--- Client/Controller/Controller.cpp
+++ Client/Controller/Controller.cpp
@@ -3,7 +3,7 @@
 #include "Objects/Actors/MainActor.hpp"
 #include "Objects/EquipableItems/FlyEngine.hpp"
 #include "Shared/KeyLayout.hpp"
-#include "Shared/GameMode.hpp"
+#include "Shared/GameStateController.hpp"
 
 Controller::Controller() : m_controlledCamera(nullptr) {}
 Controller::~Controller() {}
@@ -43,70 +43,90 @@ void Controller::stopKeyControl() {
 
 #include "Engines/Networking/NetworkController.hpp"
 void Controller::keyEvent(GLFWwindow *w, mgl::Key key, int scancode, mgl::KeyAction action, mgl::Mods mods) {
-	if (m_keys) {
-		if (action == mgl::KeyAction::press) {
-			if (key == *m_keys->move_up) {
-				NetworkController::send_control_event(NetworkController::ControlEvent::move_up, true);
-				m_actor->m_engine->accelerate_up(false);
-			} else if (key == *m_keys->move_down) {
-				NetworkController::send_control_event(NetworkController::ControlEvent::move_down, true);
-				m_actor->m_engine->accelerate_down(false);
-			} else if (key == *m_keys->move_left) {
-				NetworkController::send_control_event(NetworkController::ControlEvent::move_left, true);
-				m_actor->m_engine->accelerate_left(false);
-			} else if (key == *m_keys->move_right) {
-				NetworkController::send_control_event(NetworkController::ControlEvent::move_right, true);
-				m_actor->m_engine->accelerate_right(false);
-			} else if (key == *m_keys->anti_gravity) {
-				NetworkController::send_control_event(NetworkController::ControlEvent::anti_gravity);
-				m_actor->m_engine->activate_anti_gravity(m_actor->mass());
-			} else if (key == *m_keys->pause) {
-				GameModeController::pauseGame();
+	if (GameStateController::mode() == ProgramMode::Client && m_keys) {
+		if (GameStateController::state() == GameState::Normal) {
+			if (action == mgl::KeyAction::press) {
+				if (key == *m_keys->move_up) {
+					NetworkController::send_control_event(NetworkController::ControlEvent::move_up, true);
+					m_actor->m_engine->accelerate_up(false);
+				} else if (key == *m_keys->move_down) {
+					NetworkController::send_control_event(NetworkController::ControlEvent::move_down, true);
+					m_actor->m_engine->accelerate_down(false);
+				} else if (key == *m_keys->move_left) {
+					NetworkController::send_control_event(NetworkController::ControlEvent::move_left, true);
+					m_actor->m_engine->accelerate_left(false);
+				} else if (key == *m_keys->move_right) {
+					NetworkController::send_control_event(NetworkController::ControlEvent::move_right, true);
+					m_actor->m_engine->accelerate_right(false);
+				} else if (key == *m_keys->anti_gravity) {
+					NetworkController::send_control_event(NetworkController::ControlEvent::anti_gravity);
+					m_actor->m_engine->activate_anti_gravity(m_actor->mass());
+				} else if (key == *m_keys->pause) {
+					GameStateController::pause();
+				}
+			} else if (action == mgl::KeyAction::release) {
+				if (key == *m_keys->move_up) {
+					NetworkController::send_control_event(NetworkController::ControlEvent::move_up, false);
+					m_actor->m_engine->accelerate_up(true);
+				} else if (key == *m_keys->move_down) {
+					NetworkController::send_control_event(NetworkController::ControlEvent::move_down, false);
+					m_actor->m_engine->accelerate_down(true);
+				} else if (key == *m_keys->move_left) {
+					NetworkController::send_control_event(NetworkController::ControlEvent::move_left, false);
+					m_actor->m_engine->accelerate_left(true);
+				} else if (key == *m_keys->move_right) {
+					NetworkController::send_control_event(NetworkController::ControlEvent::move_right, false);
+					m_actor->m_engine->accelerate_right(true);
+				}
 			}
-		} else if (action == mgl::KeyAction::release) {
-			if (key == *m_keys->move_up) {
-				NetworkController::send_control_event(NetworkController::ControlEvent::move_up, false);
-				m_actor->m_engine->accelerate_up(true);
-			} else if (key == *m_keys->move_down) {
-				NetworkController::send_control_event(NetworkController::ControlEvent::move_down, false);
-				m_actor->m_engine->accelerate_down(true);
-			} else if (key == *m_keys->move_left) {
-				NetworkController::send_control_event(NetworkController::ControlEvent::move_left, false);
-				m_actor->m_engine->accelerate_left(true);
-			} else if (key == *m_keys->move_right) {
-				NetworkController::send_control_event(NetworkController::ControlEvent::move_right, false);
-				m_actor->m_engine->accelerate_right(true);
-			}
+		} else if (GameStateController::state() == GameState::Pause) {
+			//Pause controls.
+			if (action == mgl::KeyAction::press && key == *m_keys->pause)
+				GameStateController::pause();
+		} else if (GameStateController::state() == GameState::Initialization) {
+			//Loading controls.
 		}
 	}
 }
 void Controller::mouseButtonEvent(GLFWwindow *w, mgl::MouseButton button, mgl::MouseAction action, mgl::Mods mods) {
-	if (action == mgl::MouseAction::press) {
-		if (button == mgl::MouseButton::left) {
-			NetworkController::send_control_event(NetworkController::ControlEvent::left_weapon, true);
-			m_actor->activateRightWeapon();
-		} else if (button == mgl::MouseButton::right) {
-			NetworkController::send_control_event(NetworkController::ControlEvent::right_weapon, true);
-			m_actor->activateLeftWeapon();
-		}
-	} else if (action == mgl::MouseAction::release) {
-		if (button == mgl::MouseButton::left) {
-			NetworkController::send_control_event(NetworkController::ControlEvent::left_weapon, false);
-			m_actor->deactivateRightWeapon();
-		} else if (button == mgl::MouseButton::right) {
-			NetworkController::send_control_event(NetworkController::ControlEvent::right_weapon, false);
-			m_actor->deactivateLeftWeapon();
+	if (GameStateController::mode() == ProgramMode::Client) {
+		if (GameStateController::state() == GameState::Normal) {
+			if (action == mgl::MouseAction::press) {
+				if (button == mgl::MouseButton::left) {
+					NetworkController::send_control_event(NetworkController::ControlEvent::left_weapon, true);
+					m_actor->activateRightWeapon();
+				} else if (button == mgl::MouseButton::right) {
+					NetworkController::send_control_event(NetworkController::ControlEvent::right_weapon, true);
+					m_actor->activateLeftWeapon();
+				}
+			} else if (action == mgl::MouseAction::release) {
+				if (button == mgl::MouseButton::left) {
+					NetworkController::send_control_event(NetworkController::ControlEvent::left_weapon, false);
+					m_actor->deactivateRightWeapon();
+				} else if (button == mgl::MouseButton::right) {
+					NetworkController::send_control_event(NetworkController::ControlEvent::right_weapon, false);
+					m_actor->deactivateLeftWeapon();
+				}
+			}
+		} else if (GameStateController::state() == GameState::Pause) {
+			//Pause controls.
+		} else if (GameStateController::state() == GameState::Initialization) {
+			//Loading controls.
 		}
 	}
 }
 
 #include "../MyGraphicsLibrary/MGL/OpenGL/FunctionsMirror/ExperimentalFunctionsMirror.hpp"
 void Controller::mouseMoveEvent(GLFWwindow *w, double x, double y) {
-	if (!current_screen_width || !current_screen_height)
-		mgl::getWindowSize(w, &current_screen_width, &current_screen_height);
-	auto temp_x = m_controlledCamera->minX();
-	auto temp_y = m_controlledCamera->minY();
-	float tx = float(x) / current_screen_width * (m_controlledCamera->maxX() - temp_x) + temp_x;
-	float ty = float(y) / current_screen_height * (m_controlledCamera->maxY() - temp_y) + temp_y;
-	m_actor->aim(tx, ty);
+	if (GameStateController::mode() == ProgramMode::Client) {
+		if (GameStateController::state() == GameState::Normal) {
+			if (!current_screen_width || !current_screen_height)
+				mgl::getWindowSize(w, &current_screen_width, &current_screen_height);
+			auto temp_x = m_controlledCamera->minX();
+			auto temp_y = m_controlledCamera->minY();
+			float tx = float(x) / current_screen_width * (m_controlledCamera->maxX() - temp_x) + temp_x;
+			float ty = float(y) / current_screen_height * (m_controlledCamera->maxY() - temp_y) + temp_y;
+			m_actor->aim(tx, ty);
+		}
+	}
 }
