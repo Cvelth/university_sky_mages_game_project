bdd6f818ac8758f6958f45d5a3994788685b07fa
Cvelth <Cvelth.mail@gmail.com>
2018-08-19 18:45:14 +0300

    RenderInfo was restructured to control its initialization.

--- Engines/Graphics/RenderInfo.hpp
+++ Engines/Graphics/RenderInfo.hpp
@@ -1,21 +1,27 @@
 #pragma once
 namespace mgl {
 	class AbstractDrawableObject;
+	class Primitive;
+	class VertexArray;
+	enum class DataUsage;
+	class InstancingArray;
+	class InstancingMultiArray;
 }
 
 class RenderInfo {
 private:
-	mgl::AbstractDrawableObject* m_object;
+	mgl::AbstractDrawableObject *m_object;
+	bool is_initialized;
 public:
 	explicit RenderInfo();
 	virtual ~RenderInfo();
-	inline mgl::AbstractDrawableObject* operator*() {
-		return m_object;
-	}
-	inline mgl::AbstractDrawableObject* operator->() {
-		return m_object;
-	}
-	inline mgl::AbstractDrawableObject* get() {
-		return m_object;
-	}
+	virtual void addPrimitive(mgl::Primitive *v);
+	virtual void insertVertexArray(mgl::VertexArray *vao);
+	virtual void send(mgl::DataUsage const& u);
+	virtual void draw();
+	virtual void draw(mgl::InstancingArray *instances);
+	virtual void draw(mgl::InstancingMultiArray *instances);
+	virtual void clean();
+
+	virtual bool isInitialized() const { return is_initialized; }
 };
