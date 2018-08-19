ead1ccff9accf40f103b3a9214d6bc19ebf22840
Cvelth <Cvelth.mail@gmail.com>
2018-08-19 18:45:48 +0300

    IndependentObject no longer controls initialization of its RenderInfo as it's now capable of doing so itself.

--- Objects/AbstractObjects/IndependentObject.hpp
+++ Objects/AbstractObjects/IndependentObject.hpp
@@ -9,24 +9,11 @@ class IndependentObject : public IndependentObjectState {
 	friend PhysicsEngine;
 protected:
 	std::shared_ptr<RenderInfo> m_render_info;
-
-	bool m_isInitialized;
 public:
 	IndependentObject(std::shared_ptr<RenderInfo> render_info, float mass, float size_h,
 					   float size_v, float position_h, float position_v)
-		: IndependentObjectState(mass, size_h, size_v, position_h, position_v), m_render_info(render_info),
-		m_isInitialized(false) {}
+		: IndependentObjectState(mass, size_h, size_v, position_h, position_v), m_render_info(render_info) {}
 	virtual ~IndependentObject() {}
-
-	inline bool isInitialized() const {
-		return m_isInitialized;
-	}
-	inline void initializationWasSuccessfull() {
-		m_isInitialized = true;
-	}
-	inline void reinitialize() {
-		m_isInitialized = false;
-	}
 
 	inline std::shared_ptr<RenderInfo> getRenderInto() {
 		return m_render_info;
