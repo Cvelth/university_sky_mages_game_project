5f614d2baa2f661c5d9605e72779959447ad0815
Cvelth <Cvelth.mail@gmail.com>
2018-08-19 18:23:41 +0300

    Minor bug was fixed.

--- Client/ClientEntryPoint/client.hpp
+++ Client/ClientEntryPoint/client.hpp
@@ -59,6 +59,8 @@ void game_process(Objects *o, size_t &client_index) {
 		std::getline(s, string);
 		if (string == "Index") {
 			s >> client_index;
+			if (actors.size() != 0)
+				main_actor = actors[client_index];
 		} else if (string == "Map") {
 			auto temp = std::shared_ptr<Map>(MapStorage::string_to_map(data.substr(string.size() + 1)));
 			if (!temp)
