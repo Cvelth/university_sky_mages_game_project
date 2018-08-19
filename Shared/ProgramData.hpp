280f777787e26415044d5ff130dd9768f3afd5ba
Cvelth <Cvelth.mail@gmail.com>
2018-08-19 22:24:07 +0300

    Program version is now v0.3.2.374_dev. Program can now be properly built in x86 mode.

--- Shared/ProgramData.hpp
+++ Shared/ProgramData.hpp
@@ -4,5 +4,5 @@ const std::string Program_Name = "SkyMages";
 const unsigned int Program_Major_Version = 0u;
 const unsigned int Program_Minor_Version = 3u;
 const unsigned int Program_Patch_Version = 2u;
-const unsigned int Program_Build_Version = 373u;
+const unsigned int Program_Build_Version = 374u;
 const std::string Program_Version_Suffix = "dev";
