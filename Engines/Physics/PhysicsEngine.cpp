6b923843e05a820b29d89b828c48140d9f3dfd77
Cvelth <Cvelth.mail@gmail.com>
2018-08-19 21:35:32 +0300

    PhysicsEngine now uses lvalue references to store queues.

--- Engines/Physics/PhysicsEngine.cpp
+++ Engines/Physics/PhysicsEngine.cpp
@@ -3,16 +3,9 @@
 
 size_t PhysicsEngine::UpdateInterval = 16667ul;
 
-PhysicsEngine::PhysicsEngine() : m_is_initialized(false) {}
-
-PhysicsEngine::PhysicsEngine(std::function<bool(void)> const& finishFlagAccess, 
-							 MainActorQueue *actor_queue, DoubleProjectileQueue *projectile_queue, ObjectQueue *object_queue) : PhysicsEngine() {
-	initialize(finishFlagAccess, actor_queue, projectile_queue, object_queue);
-}
-
-PhysicsEngine::~PhysicsEngine() {
-	clean();
-}
+PhysicsEngine::PhysicsEngine(std::function<bool(void)> const& finishFlagAccess,
+							 MainActorQueue &actor_queue, DoubleProjectileQueue &projectile_queue, ObjectQueue &object_queue)
+	: m_actor_queue(actor_queue), m_projectile_queue(projectile_queue), m_object_queue(object_queue), m_finish_flag_access(finishFlagAccess) {}
 
 size_t PhysicsEngine::getUpdateInterval() {
 	return UpdateInterval;
@@ -21,43 +14,8 @@ size_t PhysicsEngine::getUpdateInterval() {
 void PhysicsEngine::changeUpdateInterval(size_t microseconds) {
 	UpdateInterval = microseconds;
 }
-
-void PhysicsEngine::initialize(std::function<bool()> const& finishFlagAccess, 
-							   MainActorQueue *actor_queue, DoubleProjectileQueue *projectile_queue, ObjectQueue *object_queue) {
-	m_finish_flag_access = finishFlagAccess;
-
-	if (actor_queue != nullptr)
-		m_actor_queue = actor_queue;
-	else
-		m_actor_queue = new MainActorQueue();
-	if (projectile_queue != nullptr)
-		m_projectile_queue = projectile_queue;
-	else
-		m_projectile_queue = new DoubleProjectileQueue();
-	if (object_queue != nullptr)
-		m_object_queue = object_queue;
-	else
-		m_object_queue = new ObjectQueue();
-
-	m_is_initialized = true;
-}
 void PhysicsEngine::initializeCollisionSystem(std::shared_ptr<Map> map) {
 	m_map = map;
-}
-void PhysicsEngine::addObject(std::shared_ptr<IndependentObject> object) {
-	m_object_queue->add(object);
-}
-void PhysicsEngine::removeObject(std::shared_ptr<IndependentObject> object) {
-	m_object_queue->remove(object);
-}
-void PhysicsEngine::addActor(std::shared_ptr<MainActor> actor) {
-	m_actor_queue->add(actor);
-}
-void PhysicsEngine::removeActor(std::shared_ptr<MainActor> actor) {
-	m_actor_queue->remove(actor);
-}
-void PhysicsEngine::clean() {
-	m_is_initialized = false;
 }
 #include <iostream>
 #include <chrono>
@@ -73,28 +31,27 @@ void PhysicsEngine::loop(bool destroy_engine_after_exit) {
 		auto next_tick = std::chrono::steady_clock::now() + std::chrono::microseconds(UpdateInterval);
 
 		if (GameStateController::state() == GameState::Normal) {
-			m_actor_queue->for_each([this](std::shared_ptr<MainActor> go) {
+			m_actor_queue.for_each([this](std::shared_ptr<MainActor> go) {
 				processForces(go);
 				processMovement(go, m_map);
 				if (GameStateController::mode() == ProgramMode::Server)
 					processWeaponry(go, m_projectile_queue);
 			});
-			m_projectile_queue->get().for_each([this](std::shared_ptr<ShootableObject> go) {
+			m_projectile_queue->for_each([this](std::shared_ptr<ShootableObject> go) {
 				processForces(go);
 				processMovement(go, m_map);
 			});
-			m_object_queue->for_each([this](std::shared_ptr<IndependentObject> go) {
+			m_object_queue.for_each([this](std::shared_ptr<IndependentObject> go) {
 				processForces(go);
 				processMovement(go, m_map);
 			});
 
 			if (GameStateController::mode() == ProgramMode::Server)
-				NetworkController::update_state(m_actor_queue, &m_projectile_queue->get(), m_object_queue);
+				NetworkController::update_state(m_actor_queue, m_projectile_queue.get(), m_object_queue);
 		}
 		std::this_thread::sleep_until(next_tick);
 	}
 	GameStateController::change_physics_loop_state(false);
-	clean();
 	if (destroy_engine_after_exit)
 		delete this;
 }
--- Engines/Physics/PhysicsProcessingFunctions.cpp
+++ Engines/Physics/PhysicsProcessingFunctions.cpp
@@ -63,8 +63,8 @@ void PhysicsEngine::processMovement(std::shared_ptr<IndependentObjectState> os, 
 #include "Objects/Actors/MainActor.hpp"
 #include "Objects/EquipableItems/Weapon.hpp"
 #include "Objects/ObjectState/ObjectQueue.hpp"
-void PhysicsEngine::processWeaponry(std::shared_ptr<MainActor> ma, DoubleProjectileQueue *projectile_queue) {
+void PhysicsEngine::processWeaponry(std::shared_ptr<MainActor> ma, DoubleProjectileQueue &projectile_queue) {
 	for (auto projectile : ma->shootingProcess())
 		if (projectile)
-			projectile_queue->get().add(projectile);
+			projectile_queue->add(projectile);
 }
