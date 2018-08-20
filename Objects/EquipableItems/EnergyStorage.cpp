7a3d7d59b549d669cd97ad1b3ab00bb8a81863ea
Cvelth <Cvelth.mail@gmail.com>
2018-08-20 20:21:56 +0300

    EnergyStorage::calculate_energy_loss() now checks whether storage is empty.

--- Objects/EquipableItems/EnergyStorage.cpp
+++ Objects/EquipableItems/EnergyStorage.cpp
@@ -8,5 +8,7 @@ EnergyStorage::EnergyStorage(float capacity) : EquipableItem(), m_maximum_capaci
 void EnergyStorage::calculate_energy_loss() {
 	auto time = now();
 	m_current_capacity /= exp(m_energy_percent_loss_per_second * float(time - m_energy_loss_time) / 1.e6f);
+	if (m_current_capacity < 1.f)
+		m_current_capacity = 0;
 	m_energy_loss_time = time;
 }
