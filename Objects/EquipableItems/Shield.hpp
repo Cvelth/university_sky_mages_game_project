90a13ccdaab4154c11c16d6b392a0de93eb0b016
Cvelth <Cvelth.mail@gmail.com>
2018-08-22 14:05:38 +0300

    Basic ShieldGenerator structure was implemented.

--- Objects/EquipableItems/Shield.hpp
+++ Objects/EquipableItems/Shield.hpp
@@ -1,15 +1,40 @@
 #pragma once
 #include "Objects/AbstractObjects/EquipableItem.hpp"
 
-enum class ShieldType {
-	Anti_Energy, Anti_Projectile, Anti_Bullet
-};
+class ShootableObject;
+class EnergyStorage;
+class ObjectStorage;
 class ShieldGenerator : public EquipableItem {
+	friend ObjectStorage;
 private:
-	ShieldType m_type;
-protected:
+	float m_anti_energy_efficiency;
+	float m_anti_projectile_efficiency;
+	float m_anti_bullet_efficiency;
 
+	float m_activation_energy;
+	float m_energy_per_second;
+	EnergyStorage *m_energy_source;
+
+	mutable bool is_activated;
+protected:
+	ShieldGenerator() : EquipableItem() {}
 public:
-	ShieldGenerator(ShieldType type, float mass) : EquipableItem(mass), m_type(type) {}
 	~ShieldGenerator() {}
+	void connect_to_energy_source(EnergyStorage *source) { m_energy_source = source; }
+
+	inline void activate() const {
+		is_activated = true;
+	}
+	inline void deactivate() const {
+		is_activated = false;
+	}
+	void was_hit(ShootableObject *so);
+private:
+	template <typename value_type>
+	void set_value(std::string const& name, value_type const& value);
 };
+
+template<typename value_type>
+inline void ShieldGenerator::set_value(std::string const& name, value_type const& value) {
+	static_assert("Unsupported value type");
+}
