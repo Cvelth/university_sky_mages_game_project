85e156db9494285afaf58250bb17bc0b0612f969
Cvelth <Cvelth.mail@gmail.com>
2018-08-22 14:47:06 +0300

    set_value templates were moved back to *.hpp files as their placing in *.cpp cased issues.

--- Objects/EquipableItems/Shield.cpp
+++ Objects/EquipableItems/Shield.cpp
@@ -6,30 +6,3 @@ void ShieldGenerator::was_hit(ShootableObject *so) {
 void ShieldGenerator::shield() const {
 	//To be implemented.
 }
-
-template<>
-inline void ShieldGenerator::set_value<std::string>(std::string const& name, std::string const& value) {
-	if (name == "name")
-		m_name = value;
-	else if (name == "description")
-		m_description = value;
-	else
-		throw Exceptions::UnsupportedValueException("Unsupported value was passed");
-}
-template<>
-inline void ShieldGenerator::set_value<float>(std::string const& name, float const& value) {
-	if (name == "mass")
-		addMass(value);
-	else if (name == "anti_energy_efficiency")
-		m_anti_energy_efficiency = value;
-	else if (name == "anti_projectile_efficiency")
-		m_anti_projectile_efficiency = value;
-	else if (name == "anti_bullet_efficiency")
-		m_anti_bullet_efficiency = value;
-	else if (name == "activation_energy")
-		m_activation_energy = value;
-	else if (name == "energy_per_second")
-		m_energy_per_second = value;
-	else
-		throw Exceptions::UnsupportedValueException("Unsupported value was passed");
-}
--- Objects/EquipableItems/Shield.hpp
+++ Objects/EquipableItems/Shield.hpp
@@ -35,6 +35,32 @@ private:
 	void set_value(std::string const& name, value_type const& value);
 };
 
+template<>
+inline void ShieldGenerator::set_value<std::string>(std::string const& name, std::string const& value) {
+	if (name == "name")
+		m_name = value;
+	else if (name == "description")
+		m_description = value;
+	else
+		throw Exceptions::UnsupportedValueException("Unsupported value was passed");
+}
+template<>
+inline void ShieldGenerator::set_value<float>(std::string const& name, float const& value) {
+	if (name == "mass")
+		addMass(value);
+	else if (name == "anti_energy_efficiency")
+		m_anti_energy_efficiency = value;
+	else if (name == "anti_projectile_efficiency")
+		m_anti_projectile_efficiency = value;
+	else if (name == "anti_bullet_efficiency")
+		m_anti_bullet_efficiency = value;
+	else if (name == "activation_energy")
+		m_activation_energy = value;
+	else if (name == "energy_per_second")
+		m_energy_per_second = value;
+	else
+		throw Exceptions::UnsupportedValueException("Unsupported value was passed");
+}
 template<typename value_type>
 inline void ShieldGenerator::set_value(std::string const& name, value_type const& value) {
 	static_assert("Unsupported value type");
