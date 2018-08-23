3630adfc5c806125e65e8e7c335ac01c5871c2e7
Cvelth <Cvelth.mail@gmail.com>
2018-08-23 16:53:45 +0300

    is_alive was added to Actor.

--- Objects/AbstractObjects/ActorObject.hpp
+++ Objects/AbstractObjects/ActorObject.hpp
@@ -2,7 +2,14 @@
 #include "Objects/AbstractObjects/IndependentObject.hpp"
 
 class Actor : public IndependentObject {
+protected:
+	bool m_is_alive;
 public:
-	using IndependentObject::IndependentObject;
+	Actor(std::shared_ptr<RenderInfo> render_info, float mass, float size_h,
+		  float size_v, float position_h, float position_v) 
+		: IndependentObject(render_info, mass, size_h, size_v, position_h, position_v), m_is_alive(true) {}
 	~Actor() {}
+
+	bool is_alive() const { return m_is_alive; }
+	void die() { m_is_alive = false; }
 };
