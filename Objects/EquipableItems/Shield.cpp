23d367ccf64f0d01286174aaebb5e2c3fb721952
Cvelth <Cvelth.mail@gmail.com>
2018-08-24 17:37:50 +0300

    Shield efficiency coefficient was made smaller.

--- Objects/EquipableItems/Shield.cpp
+++ Objects/EquipableItems/Shield.cpp
@@ -3,11 +3,11 @@
 float ShieldGenerator::get_efficience_coefficient(ShootableObjectType type) {
 	switch (type) {
 		case ShootableObjectType::Energy:
-			return 1.f - m_anti_energy_efficiency;
+			return 1e3f / m_anti_energy_efficiency;
 		case ShootableObjectType::Physical:
-			return 1.f - m_anti_projectile_efficiency;
+			return 1e3f / m_anti_projectile_efficiency;
 		case ShootableObjectType::Bullet:
-			return 1.f - m_anti_bullet_efficiency;
+			return 1e3f / m_anti_bullet_efficiency;
 		default:
 			throw Exceptions::UnsupportedProjectileType("Unsupported projectile has hit a shield.");
 	}
@@ -22,7 +22,7 @@ void ShieldGenerator::activate() {
 bool ShieldGenerator::was_hit(std::shared_ptr<ShootableObject> so) {
 	if (!is_activated || !m_energy_source || m_energy_source->isEmpty())
 		return false;
-	auto damage = so->damage() * so->impulse() * get_efficience_coefficient(so->type()) * 9.e+3f;
+	auto damage = so->damage() * so->impulse() * get_efficience_coefficient(so->type());
 	std::cout << damage << " joules of Energy.\n: ";
 	return m_energy_source->use(damage);
 }
@@ -35,4 +35,5 @@ void ShieldGenerator::shield() const {
 	auto time = now();
 	if (is_activated && (!m_energy_source || m_energy_source->isEmpty() || !m_energy_source->use(m_energy_per_second * float(time - m_energy_usage_time) / 1.e6f)))
 		is_activated = false;
+	m_energy_usage_time = time;
 }
