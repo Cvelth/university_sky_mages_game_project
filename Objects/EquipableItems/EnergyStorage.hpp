00124af1f9846af266dc8bcc0c182880a1396e52
Cvelth <Cvelth.mail@gmail.com>
2018-08-25 12:21:07 +0300

    Value clear/upgrage/set system was implemented for EnergyStorage.

--- Objects/EquipableItems/EnergyStorage.hpp
+++ Objects/EquipableItems/EnergyStorage.hpp
@@ -47,34 +47,73 @@ public:
 		m_current_capacity = capacity;
 	}
 private:
+	bool clear_value(std::string const& name);
+	template <typename value_type>
+	bool upgrade_value(std::string const& name, value_type const& value);
 	template <typename value_type> 
 	void set_value(std::string const& name, value_type const& value);
 };
 
+inline bool EnergyStorage::clear_value(std::string const& name) {
+	if (name == "name") {
+		m_name = "";
+		return true;
+	} else if (name == "description") {
+		m_description = "";
+		return true;
+	} else if (name == "mass") {
+		mulMass(0.0);
+		return true;
+	} else if (name == "chance_to_take_damage") {
+		m_chance_to_take_damage = 0.f;
+		return true;
+	} else if (name == "energy_usage_coefficient") {
+		m_energy_usage_coefficient = 0.f;
+		return true;
+	} else if (name == "energy_percent_loss_per_second") {
+		m_energy_percent_loss_per_second = 0.f;
+		return true;
+	}
+	return false;
+}
+
 template<>
-inline void EnergyStorage::set_value<std::string>(std::string const& name, std::string const& value) {
-	if (name == "name")
-		m_name = value;
-	else if (name == "description")
-		m_description = value;
-	else
-		throw Exceptions::UnsupportedValueException("Unsupported value was passed");
+inline bool EnergyStorage::upgrade_value<std::string>(std::string const& name, std::string const& value) {
+	if (name == "name") {
+		m_name += value;
+		return true;
+	} else if (name == "description") {
+		m_description += value;
+		return true;
+	}
+	return false;
 }
 template<>
-inline void EnergyStorage::set_value<float>(std::string const& name, float const& value) {
-	if (name == "mass")
+inline bool EnergyStorage::upgrade_value<float>(std::string const& name, float const& value) {
+	if (name == "mass") {
 		addMass(value);
-	else if (name == "chance_to_take_damage")
-		m_chance_to_take_damage = value;
-	else if (name == "energy_usage_coefficient")
-		m_energy_usage_coefficient = value;
-	else if (name == "energy_percent_loss_per_second")
-		m_energy_percent_loss_per_second = value;
-	else
-		throw Exceptions::UnsupportedValueException("Unsupported value was passed");
+		return true;
+	} else if (name == "chance_to_take_damage") {
+		m_chance_to_take_damage += value;
+		return true;
+	} else if (name == "energy_usage_coefficient") {
+		m_energy_usage_coefficient += value;
+		return true;
+	} else if (name == "energy_percent_loss_per_second") {
+		m_energy_percent_loss_per_second += value;
+		return true;
+	}
+	return false;
+}
+template<typename value_type>
+inline bool EnergyStorage::upgrade_value(std::string const& name, value_type const& value) {
+	return false;
 }
 
 template<typename value_type>
 inline void EnergyStorage::set_value(std::string const& name, value_type const& value) {
-	static_assert("Unsupported value type");
+	bool clear_flag = clear_value(name);
+	bool set_flag = upgrade_value(name, value);
+	if (!(clear_flag && set_flag))
+		throw Exceptions::UnsupportedValueException("Unsupported value was passed");
 }
