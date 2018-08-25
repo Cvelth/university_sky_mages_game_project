840b19b6550e22666a519c5dee06ba9f6dd23622
Cvelth <Cvelth.mail@gmail.com>
2018-08-25 12:35:10 +0300

    Value clear/upgrade/set system was implemented for Trinket.

--- Objects/EquipableItems/Trinket.hpp
+++ Objects/EquipableItems/Trinket.hpp
@@ -9,29 +9,56 @@ protected:
 public:
 
 private:
+	bool clear_value(std::string const& name);
+	template <typename value_type>
+	bool upgrade_value(std::string const& name, value_type const& value);
 	template <typename value_type>
 	void set_value(std::string const& name, value_type const& value);
 };
 
-template<>
-inline void Trinket::set_value<std::string>(std::string const& name, std::string const& value) {
+inline bool Trinket::clear_value(std::string const& name) {
 	if (name == "name")
-		m_name = value;
+		m_name = "";
 	else if (name == "description")
-		m_description = value;
+		m_description = "";
+	else if (name == "mass")
+		mulMass(0.0);
+	else if (name == "chance_to_take_damage")
+		m_chance_to_take_damage = 0.f;
 	else
-		throw Exceptions::UnsupportedValueException("Unsupported value was passed");
+		return false;
+	return true;
+}
+
+template<>
+inline bool Trinket::upgrade_value<std::string>(std::string const& name, std::string const& value) {
+	if (name == "name")
+		m_name += value;
+	else if (name == "description")
+		m_description += value;
+	else
+		return false;
+	return true;
 }
 template<>
-inline void Trinket::set_value<float>(std::string const& name, float const& value) {
+inline bool Trinket::upgrade_value<float>(std::string const& name, float const& value) {
 	if (name == "mass")
 		addMass(value);
 	else if (name == "chance_to_take_damage")
-		m_chance_to_take_damage = value;
+		m_chance_to_take_damage += value;
 	else
-		throw Exceptions::UnsupportedValueException("Unsupported value was passed");
+		return false;
+	return true;
 }
 template<typename value_type>
+inline bool Trinket::upgrade_value(std::string const& name, value_type const& value) {
+	return false;
+}
+
+template<typename value_type>
 inline void Trinket::set_value(std::string const& name, value_type const& value) {
-	static_assert("Unsupported value type");
+	bool clear_flag = clear_value(name);
+	bool set_flag = upgrade_value(name, value);
+	if (!(clear_flag && set_flag))
+		throw Exceptions::UnsupportedValueException("Unsupported value was passed");
 }
